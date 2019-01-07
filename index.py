10c687687883dcdba8e9251e71580c372d44514a
Robert Hubbard <rob.c.hubbard@gmail.com>
2019-01-07 17:54:42 +0000

    Add a while loop

--- index.py
+++ index.py
@@ -20,25 +20,8 @@ first_run = True
 # Copy the code under here
 
 
-# To help us in creating a task_id
-def random_number():
-    return random.randint(1000, 9999)
-
-
-# Write success and errors to log files
-def write_logs(log_data):
-    # Use today's date as the filename
-    today = datetime.today().strftime('%Y-%m-%d')
-    filename = './logs/' + str(today) + '.json'
-
-    # Open the file in append mode. This way we write all the logs from today
-    # to a single file.
-    with open(filename, 'a') as f_obj:
-        json.dump(log_data, f_obj, sort_keys=True, indent=4)
-
-
 # Create a trigger function
-def get_new_items(task_id):
+def get_new_items():
     """Get the items in the trigger app and return anything new."""
 
     # This is where we will hold our data until we return it
@@ -49,36 +32,29 @@ def get_new_items(task_id):
     try:
         # Build the url of the Airtable sheet we want to poll
         url = BASE_URL + '/Trigger%20Table?api_key=' + API_KEY
-
         # Make the request for the data
         request = urllib2.Request(url)
-
         # Open up the request to get the data
         response = urllib2.urlopen(request)
 
         # urllib2 returns a file object so we need to open it
         data = response.read()
-
         # Now we turn the string returned into a JSON object
         json_obj = json.loads(data)
 
         # Build a dictionary for logging purposes
         log_data = {
-            'task_id': task_id,
-            'trigger_success_log': {
-                'timestamp': str(datetime.now()),
-                'host': request.get_host(),
-                'request_method': request.get_method(),
-                'status_code': response.getcode(),
-                # response.info returns an instance so we turn it into a string
-                'meta_data': str(response.info()),
-                'url': request.get_full_url(),
-                'response_data': json_obj
-            }
+            'host': request.get_host(),
+            'request_method': request.get_method(),
+            'status_code': response.getcode(),
+            # response.info returns an instance so we need to turn it into a string
+            'meta_data': str(response.info()),
+            'url': request.get_full_url(),
+            'response_data': data
         }
 
-        # Write the Trigger log
-        write_logs(log_data)
+        # Let's just print the log data to the Terminal for now
+        print 'Trigger Log:\n', log_data
 
         # Iterate over the data we got back to see if anything is new
         for object in json_obj['records']:
@@ -91,41 +67,31 @@ def get_new_items(task_id):
                 if 'Email' in object['fields']:
                     # Let's add the id to the dedupe_list first
                     dedupe_list.append(object['id'])
-
                     # And then add the data to the new_items_list
                     new_items_list.append(object)
 
         # Just like Zapier we'll at least return an empty list
         return new_items_list
 
-    # Catch and log any errors
+    # If we have an error on the request let's catch it and log it
     except urllib2.HTTPError as e:
-
         # Build the log dictionary
         error_data = {
-            'task_id': task_id,
-            'trigger_error_log': {
-                'timestamp': str(datetime.now()),
-                'code': e.code,
-                'message': e.read(),
-                'info': str(e.info())
+            'code': e.code,
+            'message': e.read(),
+            'info': str(e.info())
             }
-        }
 
-        # Write the error log
-        write_logs(error_data)
-
-        # Return the empty list so the loop continues
-        return new_items_list
+        # For now print the error to the Terminal
+        print 'Trigger Error Log:\n', error_data
 
 
 # Create an action function
-def write_new_items(trigger_data, task_id):
-    """Write all the items returned by the trigger."""
+def write_new_items(trigger_data):
+    """Writes all the items returned by the trigger."""
 
     # Set our url to the Action Table in Airtable
     url = BASE_URL + '/Action%20Table'
-
     # Specify our headers for the request
     headers = {
         'Authorization': 'Bearer ' + API_KEY,
@@ -136,14 +102,8 @@ def write_new_items(trigger_data, task_id):
     for item in trigger_data:
         # The fields key holds the values of the newly created row
         fields = item['fields']
-
         # Map the data to send to our Action Table
-        values = {
-            'fields': {
-                'Email': fields['Email'],
-                'Name': fields['Name']
-            }
-        }
+        values = {'fields': {'Email': fields['Email'], 'Name': fields['Name']}}
         # Turn the data into JSON before we send it
         data = json.dumps(values)
 
@@ -151,65 +111,52 @@ def write_new_items(trigger_data, task_id):
             # Create the request and open it
             request = urllib2.Request(url, data, headers)
             response = urllib2.urlopen(request)
-
             # Read the response we got back from the request
             action_data = response.read()
 
             # Build the log dictionary
             log_data = {
-                'task_id': task_id,
-                'action_success_log': {
-                    'timestamp': str(datetime.now()),
+                'action_logs': {
                     'host': request.get_host(),
                     'request_method': request.get_method(),
                     'status_code': response.getcode(),
                     'meta_data': str(response.info()),
                     'url': request.get_full_url(),
                     'input_data': values,
-                    'response_data': json.loads(action_data)
+                    'response_data': action_data
                 }
             }
 
-            # Write the Action log
-            write_logs(log_data)
+            # Print the action response to the Terminal for now
+            print 'Action Log:\n', log_data
 
         # Catch and log any errors
         except urllib2.HTTPError as e:
+            filename = './logs/error/' + str(datetime.now()) + '.json'
 
             # Build the log dictionary
-            error_data = {
-                'task_id': task_id,
-                'action_error_log': {
-                    'timestamp': str(datetime.now()),
+            error_log = {
+                'trigger_logs': trigger_data['logs'],
+                'action_logs': {
                     'code': e.code,
                     'message': e.read(),
                     'info': str(e.info())
                 }
             }
 
-            # Write the error log
-            write_logs(error_data)
+            # For now print the error log to the Terminal
+            print 'Action Error Log:\n', error_log
 
-
-# Just to let us know the program started
-print 'Zap on'
 
 # Set up a while loop to run the Zap
 while True:
 
-    # Create a unique ID for this instance of the Zap
-    task_id = str(random_number()) + '-' + str(random_number())
-
     # Call the trigger function to see if anything new is added
-    items = get_new_items(task_id)
+    items = get_new_items()
 
-    # Only run the Action if this isn't the first time through the loop
-    # and there is something returned by the Trigger function
-    if first_run is False and len(items) > 0:
-        write_new_items(items, task_id)
-
-    # We only want to skip the Action the first time through the loop
-    first_run = False
+    # Only run the Action if there is something returned by the Trigger function
+    if len(items) > 0:
+        write_new_items(items)
 
     # Set our syncing interval
     time.sleep(6)
